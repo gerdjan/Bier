e5a4f53986eba507f595466ff5042628ce7371ca
gerdjan <christiaanwapenaar@hotmail.com>
2013-12-24 18:35:40 +0100

    added the first File

---
+++ scripts/my_scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$("#square1").click(function(){
+		$("#square1").slideUp();		
+	});
+	$("#square2").click(function(){
+		$("#square2").slideUp();		
+	});
+	$("#square3").click(function(){
+		$("#square3").slideUp();		
+	});
+	$("#square4").click(function(){
+		$("#square4").slideUp();		
+	});
+	$("#square5").click(function(){
+		$("#square5").slideUp();		
+	});
+	$("#square6").click(function(){
+		$("#square6").slideUp();		
+	});
+	$("#square7").click(function(){
+		$("#square7").slideUp();		
+	});
+	$("#square8").click(function(){
+		$("#square8").slideUp();		
+	});
+	$("#square9").click(function(){
+		$("#square9").slideUp();		
+	});
+	$("#vierkant10").click(function(){
+		$("#vierkant10").slideUp();		
+	});
+
+$("#resetButton").on('click', function() {
+		$(".square").slideDown();		
+	});
+	
+});
+
